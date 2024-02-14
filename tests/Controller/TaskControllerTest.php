03d882159a36b7facd659d14d967648d96cecce6
tonym <tony.marques@live.fr>
2024-02-15 00:53:21 +0100

    refacto for TaskControllerTest

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -5,27 +5,39 @@ namespace App\tests\Controller;
 use App\Entity\Task;
 use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class TaskControllerTest extends WebTestCase
 {
+
+    private KernelBrowser $client;
+    private ContainerInterface $container;
+    private EntityManagerInterface $entityManager;
+    private TaskRepository $taskRepository;
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function setUp():void{
+        $this->client = static::createClient();
+
+        $this->container = static::getContainer();
+
+        $this->entityManager = $this->container->get(EntityManagerInterface::class);
+
+        $this->taskRepository = $this->entityManager->getRepository(Task::class);
+
+        $this->urlGenerator = $this->container->get(UrlGeneratorInterface::class);
+    }
+
     public function testTaskListPage()
     {
-        $client = $this->createClient();
+        $this->client->request(Request::METHOD_GET, "/tasks");
 
-        $client->request(Request::METHOD_GET, "/tasks");
-
-        $container = static::getContainer();
-
-        $entityManager = $container->get(EntityManagerInterface::class);
-
-        /** @var TaskRepository */
-        $taskRepository = $entityManager->getRepository(Task::class);
-
-        $tasks = $taskRepository->findAll();
+        $tasks = $this->taskRepository->findAll();
 
         foreach($tasks as $task){
             $this->assertInstanceOf(Task::class, $task);
@@ -36,38 +48,24 @@ class TaskControllerTest extends WebTestCase
     }
 
     public function testCreateTask(){
-        $client = $this->createClient();
-        
-        $crawler = $client->request(Request::METHOD_GET, '/tasks/create');
+        $crawler = $this->client->request(Request::METHOD_GET, '/tasks/create');
         $this->assertResponseIsSuccessful();
 
         $form = $crawler->selectButton('Ajouter')->form([
-            'task[title]' => 'nouveau titre 2',
-            'task[content]' => 'nouveau contenu 2'
+            'task[title]' => 'nouveau titre 3',
+            'task[content]' => 'nouveau contenu 3'
         ]);
 
-        $client->submit($form);
+        $this->client->submit($form);
 
-        $client->followRedirect();
-        $this->assertSelectorTextContains('div.alert-success', "Superbe ! La tâche a été bien été ajoutée.");
-                
+        $this->client->followRedirect();
+        $this->assertSelectorTextContains('div.alert-success', "Superbe ! La tâche a été bien été ajoutée.");                
     }
+
     public function testEditTask(){
-        $client = $this->createClient();
-
-        $container = static::getContainer();
+        $task = $this->taskRepository->findOneBy(["id" => 1]);
         
-        $entityManager = $container->get(EntityManagerInterface::class);
-
-        /** @var TaskRepository */
-        $taskRepository = $entityManager->getRepository(Task::class);
-
-        $task = $taskRepository->findOneBy(["id" => 1]);
-
-        /** @var UrlGeneratorInterface */
-        $urlGenerator = $container->get(UrlGeneratorInterface::class);
-        
-        $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate("task_edit", ["id" => $task->getId()]));
+        $crawler = $this->client->request(Request::METHOD_GET, $this->urlGenerator->generate("task_edit", ["id" => $task->getId()]));
         $this->assertResponseIsSuccessful();
 
         $form = $crawler->selectButton('Modifier')->form([
@@ -75,10 +73,22 @@ class TaskControllerTest extends WebTestCase
             'task[content]' => 'contenu modifié'
         ]);
 
-        $client->submit($form);
+        $this->client->submit($form);
 
-        $client->followRedirect();
+        $this->client->followRedirect();
         $this->assertSelectorTextContains('div.alert-success', "Superbe ! La tâche a bien été modifiée.");
-                
+    }
+
+    public function testToggleTask(){
+        /** @var Task */
+        $task = $this->taskRepository->findOneBy(["id" => 1]);
+
+        $this->client->request(Request::METHOD_GET, $this->urlGenerator->generate("task_toggle", ["id" => $task->getId()]));
+
+        $task->toggle(!$task->isDone());
+
+        $this->client->followRedirect();
+
+        $this->assertSelectorTextContains('div.alert-success', "Superbe ! La tâche {$task->getTitle()} a bien été marquée comme faite.");
     }
 }
